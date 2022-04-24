bc95c573665471d00ff1e6c7b5db7c027a7c31f6
sarika <s.patil1@zensar.com>
2022-04-25 00:40:24 +0530

    This is my OlxAdvertise Application

---
+++ OlxAdvertiseApplication/src/main/java/com/zensar/service/OlxAdvertiseService.java
@@ -0,0 +1,25 @@
+package com.zensar.service;
+
+import java.util.List;
+
+import com.zensar.entity.Advertise;
+import com.zensar.entity.AdvertiseDetails;
+
+public interface OlxAdvertiseService {
+ Advertise addAdvertise( Advertise advertise,String username,  String password);
+	 
+	 Advertise getAdvertise( long postId, String username, String password);
+     
+	 Advertise updateAdvertise( long id,  Advertise advertises, String username, String password);
+	 
+	 List<Advertise> getAllAdvertise( String username, String password);
+	 
+	 boolean deleteAdvertise( long id, String username, String password);
+	 
+	 Advertise searchAdvertisementsByCriteria( String category, String toDate,Long filtercriteria, String fromDate);
+	 
+	 List<Advertise> searchAdvertise();
+	 
+	 List<AdvertiseDetails> advertiseDetails( long id, String username, String password);
+
+}
